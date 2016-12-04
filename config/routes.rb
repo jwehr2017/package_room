d474d558c7b58de98d600c4780c05157888dbf28
Raghu Betina <raghu@firstdraft.com>
2016-12-04 15:46:24 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "apartments#index"
   # Routes for the Employee resource:
   # CREATE
